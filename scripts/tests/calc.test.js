6fd90af70a7f662019d8bc2bd4a822929fa1fb94
jesk <86005120+jeskodes@users.noreply.github.com>
2023-09-17 15:32:56 +0100

    JEST red tests

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,16 @@
+const addition = require("/calc");
+
+describe("Calculator", () => {
+    describe("Addtion Function", () => {
+
+    });
+    describe("Subtract Function", () => {
+
+    });
+    describe("Multiply Function", () => {
+
+    });
+    describe("Divide Function", () => {
+
+    });
+})
