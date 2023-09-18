f36b9c8875dc6767a26f92b7f71aad6b37aca7a6
jesk <86005120+jeskodes@users.noreply.github.com>
2023-09-18 06:57:21 +0100

    Test 2

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -2,20 +2,21 @@ const addition = require("./calc");
 
 //red test - will fail 
 // then green - just enough code to get test to pass 
+//refactor - optimise test 
 
 describe("Calculator", () => {
-    describe("Addition Function", () => {
-        test("should return 42 for 20 + 22", () => {
-            expect(addition(20, 22)).toBe(42);
-        })
-    });
-    describe("Subtract Function", () => {
+            describe("Addition Function", () => {
+                test("should return 42 for 20 + 22", () => {
+                    expect(addition(20, 22)).toBe(42);
+                });
+                test("should return 73 for 42 + 31", () => {
+                    expect(addition(42, 31)).toBe(73);
+                });
 
-    });
-    describe("Multiply Function", () => {
+                describe("Multiply Function", () => {
 
-    });
-    describe("Divide Function", () => {
+                });
+                describe("Divide Function", () => {
 
-    });
-})
+                });
+            })})
